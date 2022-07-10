67711be3dd87d8c8e1312276bcae832ca5ff0565
Sven <67136317+Sven-Bo@users.noreply.github.com>
2022-07-10 12:27:14 +0200

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# Create a advanced GUI app with PySimpleGUI (Full Tutorial)
+
+In this tutorial, I'm going to walk you through the process of creating a GUI app with Python. I'll show you how to create an advanced Graphical User Interface complete with buttons, input boxes, user settings and a custom menu bar. I'll start with a simple GUI app that has just two buttons. Then, we'll create a more advanced GUI app to retrieve and save user settings. Finally, we will implement a custom menu bar and convert our code into a standalone executable file.<br/>
+
+In the final application, you can convert a selected Excel sheet into a CSV file. Additionally, you could specify the separator & decimal value for the CSV file.
+
+## Features
+- Load/Save user settings
+- Custom Toolbar
+- Convert Excel Files to CSV
+
+# Screenshots
+[Main Window](/main_window.jpg?raw=true "Main Window")
+[Settings Window](/settings_window.jpg?raw=true "Settings Window")
+
+## Video Tutorial
+..will follow soon!
+[![YouTube Video](https://img.youtube.com/vi/XXX/0.jpg)](https://youtu.be/XXX)
+
+## Requirements
+Install the dependecies with pip
+```
+openpyxl==3.0.10
+pandas==1.3.5
+PySimpleGUI==4.60.1
+```
+
+## Usage
+```python
+# [Vanilla Terminal] Run the application
+python main.py
+```
+
+## About Me
+- Sven from Coding Is Fun
+- YouTube: https://youtube.com/c/CodingIsFun
+- Website: https://pythonandvba.com
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/X7X47Q0EG)
+
+## Feedback
+If you have any feedback, please reach out to me at contact@pythonandvba.com
+![Logo](https://www.pythonandvba.com/banner-img)
---
+++ main.py
@@ -0,0 +1,112 @@
+from pathlib import Path  # core python module
+
+import pandas as pd  # pip install pandas openpyxl
+import PySimpleGUI as sg  # pip install pysimplegui
+
+
+def is_valid_path(filepath):
+    if filepath and Path(filepath).exists():
+        return True
+    sg.popup_error("Filepath not correct")
+    return False
+
+
+def display_excel_file(excel_file_path, sheet_name):
+    df = pd.read_excel(excel_file_path, sheet_name)
+    filename = Path(excel_file_path).name
+    sg.popup_scrolled(df.dtypes, "=" * 50, df, title=filename)
+
+
+def convert_to_csv(excel_file_path, output_folder, sheet_name, separator, decimal):
+    df = pd.read_excel(excel_file_path, sheet_name)
+    filename = Path(excel_file_path).stem
+    outputfile = Path(output_folder) / f"{filename}.csv"
+    df.to_csv(outputfile, sep=separator, decimal=decimal, index=False)
+    sg.popup_no_titlebar("Done! :)")
+
+
+def settings_window(settings):
+    # ------ GUI Definition ------ #
+    layout = [[sg.T("SETTINGS")],
+              [sg.T("Separator"), sg.I(settings["CSV"]["separator"], s=1, key="-SEPARATOR-"),
+               sg.T("Decimal"), sg.Combo(settings["CSV"]["decimal"].split("|"),
+                                   default_value=settings["CSV"]["decimal_default"],
+                                   s=1, key="-DECIMAL-"),
+               sg.T("Sheet Name:"), sg.I(settings["EXCEL"]["sheet_name"], s=20, key="-SHEET_NAME-")],
+              [sg.B("Save Current Settings", s=20)]]
+
+    window = sg.Window("Settings Window", layout, modal=True, use_custom_titlebar=True)
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+        if event == "Save Current Settings":
+            # Write to ini file
+            settings["CSV"]["separator"] = values["-SEPARATOR-"]
+            settings["CSV"]["decimal_default"] = values["-DECIMAL-"]
+            settings["EXCEL"]["sheet_name"] = values["-SHEET_NAME-"]
+
+            # Display success message & close window
+            sg.popup_no_titlebar("Settings saved!")
+            break
+    window.close()
+
+
+def main_window():
+    # ------ Menu Definition ------ #
+    menu_def = [["Toolbar", ["Command 1", "Command 2", "---", "Command 3", "Command 4"]],
+                ["Help", ["Settings", "About", "Exit"]]]
+
+
+    # ------ GUI Definition ------ #
+    layout = [[sg.MenubarCustom(menu_def, tearoff=False)],
+              [sg.T("Input File:", s=15, justification="r"), sg.I(key="-IN-"), sg.FileBrowse(file_types=(("Excel Files", "*.xls*"),))],
+              [sg.T("Output Folder:", s=15, justification="r"), sg.I(key="-OUT-"), sg.FolderBrowse()],
+              [sg.Exit(s=16, button_color="tomato"),sg.B("Settings", s=16), sg.B("Display Excel File", s=16), sg.B("Convert To CSV", s=16)],]
+
+    window_title = settings["GUI"]["title"]
+    window = sg.Window(window_title, layout, use_custom_titlebar=True)
+
+    while True:
+        event, values = window.read()
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            break
+        if event == "About":
+            window.disappear()
+            sg.popup(window_title, "Version 1.0", "Convert Excel files to CSV", grab_anywhere=True)
+            window.reappear()
+        if event in ("Command 1", "Command 2", "Command 3", "Command 4"):
+            sg.popup_error("Not yet implemented")
+        if event == "Display Excel File":
+            if is_valid_path(values["-IN-"]):
+                display_excel_file(values["-IN-"], settings["EXCEL"]["sheet_name"])
+        if event == "Settings":
+            settings_window(settings)
+        if event == "Convert To CSV":
+            if (is_valid_path(values["-IN-"])) and (is_valid_path(values["-OUT-"])):
+                convert_to_csv(
+                    excel_file_path=values["-IN-"],
+                    output_folder=values["-OUT-"],
+                    sheet_name=settings["EXCEL"]["sheet_name"],
+                    separator=settings["CSV"]["separator"],
+                    decimal=settings["CSV"]["decimal"],
+                )
+    window.close()
+
+
+if __name__ == "__main__":
+    SETTINGS_PATH = Path.cwd()
+    # create the settings object and use ini format
+    settings = sg.UserSettings(
+        path=SETTINGS_PATH, filename="config.ini", use_config_file=True, convert_bools_and_none=True
+    )
+    theme = settings["GUI"]["theme"]
+    font_family = settings["GUI"]["font_family"]
+    font_size = int(settings["GUI"]["font_size"])
+    sg.theme(theme)
+    sg.set_options(font=(font_family, font_size))
+    main_window()
+
+
+
+
